b8d56ec8450a7d3ba2e28c7c8274bff0a6de0da5
MikDra1 <mikolajkuleczka@gmail.com>
2024-07-23 17:22:45 +0200

    Second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
         Cyan: "hsl(180, 62%, 55%)",
         Orange: "hsl(34, 97%, 64%)",
         Blue: "hsl(212, 86%, 64%)",
-
         veryDarkBlue: "hsl(234, 12%, 34%)",
         grayishBlue: "hsl(229, 6%, 66%)",
         veryLightGray: "hsl(0, 0%, 98%)",
